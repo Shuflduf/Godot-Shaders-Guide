9ed61de39c54bea6135f6d1350cdb37fc3922b85
Shuflduf <shuflduf@shuflduf.xyz>
2025-06-27 18:55:39 -0600

    ajslkjaoi

--- Page/src/routes/+page.svelte
+++ Page/src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	// const engine = new Engine({});
+	const cnvs = document.querySelector('#canvas');
+	const wasm = '/godot_exports/godot';
+	const pck = '/godot_exports/godot.pck';
+	const engine = new Engine({
+		executable: wasm,
+		mainPack: pck,
+		canvas: cnvs,
+		canvasResizePolicy: 0
+	});
+	engine.startGame();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<canvas id="canvas"></canvas>
